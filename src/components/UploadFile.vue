3d4b31060c4c7a59d099b09d1a6129a24970a358
陈康 <kangchen@sf-express.com>
2019-01-09 09:30:10 +0800

    2019年1月9日09:30:01早上备份

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -4,19 +4,23 @@
         <div class="content">
             <Form :model="formItem" :label-width="80">
                 <FormItem label="标题" >
-                    <Input v-model="formItem.input" placeholder="请输入标题..." class="inp"></Input>
+                    <Input v-model="formItem.title" placeholder="请输入标题..." class="inp"></Input>
                 </FormItem>
                 <FormItem label="分类" >
-                    <Select v-model="formItem.select" class="inp">
-                        <Option value="beijing">New York</Option>
-                        <Option value="shanghai">London</Option>
-                        <Option value="shenzhen">Sydney</Option>
+                    <Select v-model="formItem.category" class="inp">
+                        <Option value="beijing">Beijing</Option>
+                        <Option value="shanghai">Shanghai</Option>
+                        <Option value="shenzhen">Chendu</Option>
                     </Select>
                 </FormItem>
-                <FormItem label="Text">
+                <FormItem label="概要" >
+                    <Input v-model="formItem.outline" placeholder="请输入内容概要..."></Input>
+                </FormItem>
+                <FormItem label="图片">
                     <Upload
                         multiple
                         :on-success="uploadSuccess"
+                        :on-remove="removeUpload"
                         type="drag"
                         name="multipartFile"
                         action="/upload/uploadImg">
@@ -27,10 +31,10 @@
                     </Upload>                
                 </FormItem>
                 <FormItem label="文件描述">
-                    <Input v-model="formItem.textarea" type="textarea" :autosize="{minRows: 5,maxRows: 10}" placeholder="Enter something..."></Input>
+                    <Input v-model="formItem.content" type="textarea" :autosize="{minRows: 5,maxRows: 10}" placeholder="Enter something..."></Input>
                 </FormItem>
                 <FormItem>
-                    <Button type="warning">Submit</Button>
+                    <Button type="warning" @click="submitImage">Submit</Button>
                     <Button style="margin-left: 8px">Cancel</Button>
                 </FormItem>
             </Form>
@@ -48,19 +52,16 @@ form .inp{
 }
 </style>
 <script>
+import {isEmptyOrUndefined,tipMessage} from '../js/util'
 export default {
         data () {
             return {
                 formItem: {
-                    input: '',
-                    select: '',
-                    radio: 'male',
-                    checkbox: [],
-                    switch: true,
-                    date: '',
-                    time: '',
-                    slider: [20, 50],
-                    textarea: ''
+                    title: '',
+                    outline: '',
+                    content: '',
+                    uploadId: '',
+                    category: ''                    
                 }              
             }
         },
@@ -68,9 +69,65 @@ export default {
             
         },
         methods: {
-            uploadSuccess(response, file){
-                console.log(response);
-            }
+            // 提示信息
+            tipMessage(type,msg){
+                switch(type){
+                    case 'info':
+                        this.$Message.info({
+                            content: msg,
+                            duration: 5
+                        });
+                        break;
+                    case 'warning':
+                        this.$Message.warning({
+                            content: msg,
+                            duration: 5
+                        });
+                        break;
+                    case 'success':
+                        this.$Message.success({
+                            content: msg,
+                            duration: 5
+                        });
+                        break;
+                    case 'error':
+                        this.$Message.error({
+                            content: msg,
+                            duration: 5
+                        });
+                        break;
+                }
+            },
+            uploadSuccess(response){
+                let data = response.data;
+                if(response.status === 'succ'){
+                    this.formItem.uploadId = data.id;
+                }
+            },
+            removeUpload(response, file){
+                let data = response.response.data;
+                this.$axios.post('/upload/deleteUploadImg?id=' + data.id).then(rs => {
+                    console.log(rs);
+                });  
+            },
+            submitImage(){
+                if(isEmptyOrUndefined(this.formItem.uploadId)){
+                    this.tipMessage("warning", "请先上传附件");
+                    return;
+                }
+                this.$axios.post('/image/addImage',{
+                    'title':this.formItem.title,
+                    'outline':this.formItem.outline,
+                    'content':this.formItem.content,
+                    'category':this.formItem.category,
+                    'uploadFile':{
+                        'id':this.formItem.uploadId
+                    }
+                }).then(rs => {
+                    console.log(rs);
+                });
+            },
+            
         }
     }
 </script>
---
+++ src/js/util.js
@@ -0,0 +1,5 @@
+// 判断是否是空
+export function isEmptyOrUndefined(obj){
+    return (obj == '' || obj == null || obj == undefined || obj.length == 0) ? true : false;
+};
+
