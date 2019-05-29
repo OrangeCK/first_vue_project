ca9707579c02122396c4acdf547d6e076694ea54
陈康 <kangchen@sf-express.com>
2019-05-29 19:14:08 +0800

    实现markdown的图片上传

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -39,7 +39,7 @@
                     </Upload>
                 </FormItem>
                 <FormItem label="正文：">
-                        <mavon-editor v-model="markdownEdit.value" style="height:500px;" @change="changeData"/>
+                        <mavon-editor ref=md v-model="markdownEdit.value" :toolbars="toolbars" style="height:500px;" @change="changeData" @imgAdd="$imgAdd" />
                 </FormItem>
             </Form>
         </div>
@@ -74,7 +74,38 @@ export default {
                     imageUrl: null,
                     categoryName: '',
                     categoryId: '',
-                }              
+                },
+                toolbars:{
+                    bold: true, // 粗体
+                    italic: true, // 斜体
+                    header: true, // 标题
+                    underline: true, // 下划线
+                    strikethrough: true, // 中划线
+                    mark: true, // 标记
+                    superscript: true, // 上角标
+                    subscript: true, // 下角标
+                    quote: true, // 引用
+                    ol: true, // 有序列表
+                    ul: true, // 无序列表
+                    link: true, // 链接
+                    imagelink: true, // 图片链接
+                    code: true, // code
+                    table: true, // 表格
+                    fullscreen: true, // 全屏编辑
+                    readmodel: true, // 沉浸式阅读
+                    htmlcode: true, // 展示html源码
+                    help: true, // 帮助
+                    undo: true, // 上一步
+                    redo: true, // 下一步
+                    trash: false, // 清空
+                    save: false, // 保存（触发events中的save事件）
+                    navigation: true, // 导航目录
+                    alignleft: true, // 左对齐
+                    aligncenter: true, // 居中
+                    alignright: true, // 右对齐
+                    subfield: true, // 单双栏模式
+                    preview: true, // 预览
+                }           
             }
         },
         created(){
@@ -88,6 +119,22 @@ export default {
             this.headers = JSON.parse(jsonStr);
         },
         methods: {
+            // markdown中上传图片
+            $imgAdd(pos, $file){
+                 // 第一步.将图片上传到服务器.
+                var formdata = new FormData();
+                formdata.append('multipartFile', $file);
+                this.$axios({
+                    url: '/orangeblog/aliOss/uploadToOss',
+                    method: 'post',
+                    data: formdata,
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                }).then(response => {
+                    var url = response.data.data;
+                    // 第二步.将返回的url替换到文本原位置![...](0) -> ![...](url)
+                    this.$refs.md.$img2Url(pos, url);
+                })
+            },
             uploadSuccess(response){
                 let data = response.data;
                 if(response.success == true){
@@ -133,7 +180,6 @@ export default {
             },
             getImage(id){
                 this.$axios.post('/image/imageDetail?id=' + id).then(rs => {
-                    console.log(rs);
                     let data = rs.data;
                     this.formItem.id = data.data.id;
                     this.formItem.title = data.data.title;
